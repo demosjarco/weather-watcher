3f0fcbf8174db8f414a8921a2cf9da003bbe08b8
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2023-09-18 14:47:28 -0700

    Create types.d.ts

---
+++ src/types.d.ts
@@ -0,0 +1,9 @@
+export type Bindings = Record<string, any> & {
+	GIT_HASH?: string;
+	NODE_ENV: string;
+
+	ACCUWEATHER_API_KEY: string;
+
+	HISTORICAL_WEATHER_COMFORT?: AnalyticsEngineDataset;
+	DISCORD_WEATHER_WATCHER_INFO: D1Database;
+};
